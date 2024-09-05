5d0cecef28ca53497c32be0ce0508953247f5ac4
shifttag <ehdgus0837@naver.com>
2024-09-05 15:53:49 +0900

    20240905

--- src/chapter04/Object04.java
+++ src/chapter04/Object04.java
@@ -35,6 +35,11 @@ class Book {
 	// 참조 타입: null
 	
 	// >> 생략 시 클래스명과 동일하면서 매개변수가 없는 메서드
+	Book(String title, String publisher, int pageNumber) {	// 매개변수로 받은 데이터로 속성 초기화
+		this.title = title;
+		this.publisher = publisher;
+		this.pageNumber = pageNumber;
+	}
 }
 
 
@@ -43,7 +48,8 @@ public class Object04 {
 		// 위에서 정의한 클래스를 사용하여 객체 인스턴스화
 		Person person = new Person("홍동현", 25);
 		
-		Book myBook = new Book();
+		Book myBook = new Book("백설공주", "코리아it아카데미", 50);
+		Book anotherBook = new Book("신데렐라", "에이원플라자", 100);
 		
 		
 		// 속성 출력
@@ -54,7 +60,17 @@ public class Object04 {
 		System.out.println(myBook.publisher);
 		System.out.println(myBook.title);
 		
+		System.out.println(anotherBook.pageNumber);
+		System.out.println(anotherBook.publisher);
+		System.out.println(anotherBook.title);
+		
 		// 메서드 호출
 		person.display();
+		
+		myBook.displayInfo();	// 백설공주코리아it아카데미
+		anotherBook.displayInfo(); 	// 신데렐라에이원플라자
+		
+		myBook.pageNumber = 1234;
+		System.out.println(myBook.pageNumber); 	// 1234
 	}
 }
