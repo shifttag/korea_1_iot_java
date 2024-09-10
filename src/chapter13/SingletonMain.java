c015f2311d5d79a2ad47b68a2939923f3af58687
shifttag <ehdgus0837@naver.com>
2024-09-10 16:48:34 +0900

    20240910

---
+++ src/chapter13/SingletonMain.java
@@ -0,0 +1,17 @@
+package chapter13;
+
+public class SingletonMain {
+	public static void main(String[] args) {
+		Singleton singleton1 = Singleton.getInstance();
+		System.out.println(singleton1);	// chapter13.Singleton@1e80bfe8
+		
+		Singleton singleton2 = Singleton.getInstance();
+		System.out.println(singleton2); // chapter13.Singleton@1e80bfe8
+		
+		if (singleton1 == singleton2) {
+			System.out.println("동일한 인스턴스");
+		} else {
+			System.out.println("다른 인스턴스");
+		}
+	}
+}
