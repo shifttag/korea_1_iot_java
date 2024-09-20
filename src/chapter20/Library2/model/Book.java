1a51b43d757609bd38f0cd4ea1ec909730234872
shifttag <ehdgus0837@naver.com>
2024-09-20 11:10:25 +0900

    20240920

--- src/chapter20/Library2/model/Book.java
+++ src/chapter20/Library2/model/Book.java
@@ -1,5 +1,42 @@
 package chapter20.Library2.model;
 
-public class Book {
+import lombok.Data;
 
+@Data
+public class Book {
+	private String title;
+	private String author;
+	private Boolean isBorrowed;	// 책의 대여 상태를 나타내는 필드(false - 미대여)
+	
+	// 생성자
+	public Book(String title, String author, Boolean isBorrowed) {
+		this.title = title;
+		this.author = author;
+		this.isBorrowed = isBorrowed;
+	}
+	
+	/*
+	lombok에 @Data로 대체 가능
+	// Getters
+	public String getTitle() {
+		return title;
+	}
+	public String getAuthor() {
+		return author;
+	}
+	public Boolean getIsBorrowed() {
+		return isBorrowed;
+	}
+	
+	// Setters
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public void setAuthor(String author) {
+		this.author = author;
+	}
+	public void setTtitle(Boolean isBorrwed) {
+		this.isBorrowed = isBorrowed;
+	}
+	*/
 }
