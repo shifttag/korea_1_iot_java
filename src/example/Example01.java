5e6855873815aeb818fabd3256b7d1ccb3d659d7
shifttag <ehdgus0837@naver.com>
2024-09-23 17:18:50 +0900

    20240923

--- src/example/Example01.java
+++ src/example/Example01.java
@@ -31,6 +31,6 @@ class Child extends Parent {
 public class Example01 {
     public static void main(String[] args) {
         Child obj = new Child();
-        System.out.println(obj.x);
+        System.out.println(obj.getX());
     }
 }
