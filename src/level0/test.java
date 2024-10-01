ed57e4f9213fcb758901371bc464850150ef7334
shifttag <ehdgus0837@naver.com>
2024-10-01 17:41:48 +0900

    20241001

---
+++ src/level0/test.java
@@ -0,0 +1,45 @@
+package level0;
+
+class Computer {
+	// 필드 선언
+	
+	
+	
+	
+	// private 생성자, 빌더로만 객체 생성 가능
+	
+	
+	
+	
+	// ComputerBuilder 클래스 정의 (내부 static 클래스)
+	public static class ComputerBuilder {
+		// 필수 필드
+		
+		
+		
+		// 선택적 필드
+		
+		
+		
+		// 생성자에서 필수 필드 초기화
+		
+		
+		
+		// 선택적 필드를 위한 메서드
+		
+		
+	}
+	
+	
+	// 필드 출력 메서드
+}
+
+public class test {
+	public static void main(String[] args) {
+		// 빌더 패턴을 사용해 Computer 객체 생성
+		
+		
+		
+		// 컴퓨터 사양 출력
+	}
+}
